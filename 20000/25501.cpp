4f3a35445598cac1f3f5cdb35e897ebceff9a307
james98k <eunhokim4023@daum.net>
2023-01-28 10:25:22 +0000

    25501 solved

--- 20000/25501.cpp
+++ 20000/25501.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
+#include <array>
+#include <cstring>
 
 using namespace std;
 
-int recursion(string s, int l, int r, int * count){
+int recursion(const char * s, int l, int r, int * count){
+	*count += 1;
     if(l >= r) return 1;
     else if(s[l] != s[r]) return 0;
-    else {
-		*count += 1;
-		return recursion(s, l+1, r-1, count);	
-	}
+    else return recursion(s, l+1, r-1, count);	
 }
 
 
-int isPalindrome(string s, int * count){
-	return recursion(s, 0, s.length()-1, count);
+int isPalindrome(const char * s, int * count){
+	return recursion(s, 0, strlen(s)-1, count);
 }
 
 int main(){
@@ -26,13 +26,20 @@ int main(){
 	std::cin>>loopCount;
 	
 	for(int i = 0; i < loopCount; i++){
-		string inputString;
+		// std::array<char, 1000> inputString;
+		char * inputString;
 		
 		std::cin>>inputString;
+		// std::cout<<inputString;
+		// printf("%s", inputString);
+	
 		
-		int palindromeStatus = recursion(inputString, 0, inputString.length()-1, recurrCount);
+		int palindromeStatus = recursion(inputString, 0, strlen(inputString)-1, recurrCount);
 		
-		std::cout<<palindromeStatus<<" "<<recurrCount;
+		std::cout<<palindromeStatus<<" "<<*recurrCount<<endl;
 		
+		*recurrCount = 0;
+		
+		// inputString[0] = {0,};
 	}
 }
