44f2c2e402b160b469208c20788783eb38685e83
james98k <54566288+james98k@users.noreply.github.com>
2023-10-06 22:23:44 +0900

    2751 working

---
+++ 2000/2751.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void quick_sort(){
+
+}
+
+int main(){
+
+    int a;
+    std::cin>>a;
+    vector<int> list(a);
+
+    for(int i = 0; i < a; i++){
+        std::cin>>list[i];
+    }
+
+    std::cout<<list<<;
+}
