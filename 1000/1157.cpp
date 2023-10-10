60315ffd75272511dfd313c023d6569afdeb8e46
james98k <54566288+james98k@users.noreply.github.com>
2023-10-10 14:09:03 +0000

    1157 solved

--- 1000/1157.cpp
+++ 1000/1157.cpp
@@ -1,31 +1,68 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+class Node{
+    public:
+    int  count;
+    char  value;
+    Node(char v, int c){
+        count = c;
+        value = v;
+    }
+    void addCount(){
+        count++;
+    }
+}; 
+
 int main(){
 
-    int max;
-    string str;
-    getline(cin, str);
-    
-    vector<int> count(str.length(), 0);
-    
+    string input = "";
+    std::cin>>input;
 
-    for(int i = 0; i < str.length(); i++){
-        for(int j = 0; j < str.length(); j++){
-            if(str[i] == str[j]){
-                count[i]++;
+    vector<Node> v;
 
+
+    for(int i = 0 ; i < input.length(); i++){
+        bool hasChar = false;
+        char targetChar = toupper(input[i]);
+
+        //1. 비어있는 벡터
+        if(v.size() == 0){
+            v.push_back(Node(targetChar, 1));
+            continue;
+        }
+        //2. 같은 값을 찾을 경우 ++
+        for(auto &node : v){
+            if(node.value == targetChar){
+                node.addCount();
+                hasChar = true;
+                break;
             }
         }
-        if(str[i] > str[i-1]){
-            max = i;
+        //3. 없는 값을 경우 push back
+        if(!hasChar){
+            v.push_back(Node(targetChar, 1));
         }
     }
 
-    std::cout<<max;
-    std::cout<<str[max-1];
-    return 0;
+    Node temp = Node(' ', 0);
+    bool flag = false;
+
+    for(auto node : v){
+        if(node.count == temp.count)
+            flag = true;
+        
+        else if(node.count > temp.count){
+            temp = node;    
+            flag = false;
+        }
+    }
+
+    if(flag) std::cout<<"?";
+    else std::cout<<temp.value;
+    
 }
