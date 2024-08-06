501dd0657df85775fb34c265c873eada37e7be12
james98k <eunhokim4023@daum.net>
2024-08-06 12:21:16 +0900

    31403 solved

--- 30000/31403.cpp
+++ 30000/31403.cpp
@@ -7,20 +7,8 @@ int main(){
     string a, b, c;
 
     std::cin>>a>>b>>c;
-
-
-
+    
     std::cout<<stoi(a)+ stoi(b) - stoi(c) <<endl;
-
-    //문자열 설정 -> 10 단위의 수가 나왔을때는 문제해결이 안됨. 
     a += b;
-
     std::cout<<stoi(a) - stoi(c)<<endl;
-
-    
-
-    
-
-
-
 }
